6294645a7302eca4560d23076803f722b984b395
Josphat <josphatmaphololo707@gmail.com>
2024-04-17 02:06:27 -0700

    task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,3 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
-0x0B-python-input_output/1-write_file.py
-
-#!/usr/bin/python3
